c0c21ddbfeab3b3cc54b7d960d62bab820e9071d
JacoboSilvestreKiroshi <168037223+JacoboSilvestreKiroshi@users.noreply.github.com>
2024-09-09 12:59:52 -0500

    xd

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 	function conexion(){
 
-	$host = "host=dpg-cr6g053v2p9s7392u7d0-a";
+	$host = "host=dpg-cr6g053v2p9s7392u7d0-a.oregon-postgres.render.com";
 	$port = "port=5432";
 	$dbname = "dbname=dbcprueba;
 	$user = "user=dbcprueba_user";
